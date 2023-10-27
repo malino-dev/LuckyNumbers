c28c3fa689a86d8a070b8e657ad995e66055aef1
user1 <user1@DESKTOP-ALD5NV3>
2023-10-27 14:05:01 +0200

    Add project files.

---
+++ LuckyNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿int lower, upper;
+
+/*Console.Write("Lower bound: ");
+if (!int.TryParse(Console.ReadLine() ?? "1", out lower)) lower = 2;*/
+lower = 1;
+
+/*Console.Write("Upper bound: ");
+if (!int.TryParse(Console.ReadLine() ?? "1", out upper)) upper = 100;*/
+upper = 100;
+
+int?[] numbers = Enumerable.Range(lower, upper - lower).Select(x => (int?) x).ToArray();
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] is >= -1 and <= 1) continue;
+    if (numbers[i] is null) continue;
+
+    int k = 0;
+    int number = (int)numbers[i]!;
+
+    for(int j = 0; j < numbers.Length; j++)
+    {
+        if (numbers[j] is null) continue;
+
+        if (k != 0 && (k + lower) % number == 0)
+        {
+            numbers[j] = null;
+        }
+
+        k++;
+    }
+}
+
+IEnumerable<int> result = numbers
+    .Where(x => x is not null)
+    .Select(x => (int)x!);
+
+Console.WriteLine();
+Console.WriteLine("Lucky numbers: {0}", string.Join("\n", result));
